450acefa9ce3acf40b54855ec7bfdbde43db3aae
Jean-Charles Roger <jeancharles.roger@gmail.com>
2013-02-23 15:25:41 +0100

    Adds GPLv3 header.

--- hannah/src-tests/org/openflexo/hannah/ConflictingTests.java
+++ hannah/src-tests/org/openflexo/hannah/ConflictingTests.java
@@ -1,3 +1,19 @@
+/* *********************************************************************
+ *  This file is part of Hannah.
+ *
+ *  Hannah is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Hannah is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Hannah.  If not, see <http://www.gnu.org/licenses/>.
+ * ******************************************************************* */
 package org.openflexo.hannah;
 
 import static org.openflexo.hannah.TestUtil.assertContents;
@@ -6,6 +22,7 @@ import static org.openflexo.hannah.TestUtil.writeFile;
 import java.io.File;
 
 import org.junit.Test;
+import org.openflexo.hannah.Conflict.Resolution;
 
 public class ConflictingTests {
 
@@ -22,7 +39,7 @@ public class ConflictingTests {
 		VersionnedFileGenerator generator = createGenerator("oneFile1");
 		generator.start(TestUtil.noModification);
 		generator.generate("file1.txt", "abc\ndef\nijk\n");
-		generator.end(TestUtil.noConflict);
+		generator.end(Resolution.USER);
 		
 		assertContents(generator, "file1.txt", "abc\ndef\nijk\n");
 		
@@ -31,7 +48,7 @@ public class ConflictingTests {
 		
 		generator.start(ModificationHandler.accept);
 		generator.generate("file1.txt", "abc\nfed\nijk\n");
-		generator.end(ConflictHandler.user);
+		generator.end(Resolution.USER);
 		
 		assertContents(generator, "file1.txt", "abc\nddd\nijk\n");
 	}
@@ -41,7 +58,7 @@ public class ConflictingTests {
 		VersionnedFileGenerator generator = createGenerator("oneFile2");
 		generator.start(TestUtil.noModification);
 		generator.generate("file1.txt", "abc\ndef\nijk\n");
-		generator.end(TestUtil.noConflict);
+		generator.end(Resolution.GENERATION);
 		
 		assertContents(generator, "file1.txt", "abc\ndef\nijk\n");
 		
@@ -50,10 +67,30 @@ public class ConflictingTests {
 		
 		generator.start(ModificationHandler.accept);
 		generator.generate("file1.txt", "abc\nfed\nijk\n");
-		generator.end(ConflictHandler.generation);
+		generator.end(Resolution.GENERATION);
 		
 		assertContents(generator, "file1.txt", "abc\nfed\nijk\n");
 	}
 	
 
+	@Test
+	public void testOneFile3() throws Exception {
+		VersionnedFileGenerator generator = createGenerator("oneFile3");
+		generator.start(TestUtil.noModification);
+		generator.generate("file1.txt", "abc\ndef\nijk\nlmn\nopq\nrst\nuvw\n");
+		generator.end(Resolution.USER);
+		
+		assertContents(generator, "file1.txt", "abc\ndef\nijk\nlmn\nopq\nrst\nuvw\n");
+		
+		writeFile(generator, "file1.txt", "abc\nddd\nijk\nlmn\nooo\nrst\nuvw\n");
+		assertContents(generator, "file1.txt", "abc\nddd\nijk\nlmn\nooo\nrst\nuvw\n");
+		
+		generator.start(ModificationHandler.accept);
+		generator.generate("file1.txt", "abc\ndef\nijk\nlmn\nopq\nrst\nuvw\nxyz\n");
+		generator.end(Resolution.USER);
+		
+		assertContents(generator, "file1.txt", "abc\nddd\nijk\nlmn\nooo\nrst\nuvw\nxyz\n");
+	}
+	
+	
 }
